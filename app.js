56eec3fa95ebaf97d6541c4e4a3d91de63888728
lundyc0917 <lundyc0917@gmail.com>
2021-01-12 21:31:41 -0500

    updated app.js

--- app.js
+++ app.js
@@ -10,9 +10,125 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// Manager Info question Array
+const managerInfo = [
+        {
+            type: "input",
+            name: "managerName",
+            message: "Enter Manager's Name: "
+        },
+        {
+            type: "input",
+            name: "managerID",
+            message: "Enter Manager's ID: "
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "Enter Manager's Email: "
+        },
+        {
+            type: "input",
+            name: "managerOfficeNumber",
+            message: "Enter Manager's Office Number: "
+        }
+    ];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// Engineer Info quesiton Array
+const engineerInfo = [
+        {
+            type: "input",
+            name: "engineerName",
+            message: "Enter Engineer's Name: "
+        },
+        {
+            type: "input",
+            name: "engineerID",
+            message: "Enter Engineer's ID: "
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "Enter Engineer's Email: "
+        },
+        {
+            type: "input",
+            name: "engineerGithub",
+            message: "Enter Engineer's GitHub User Name: "
+        }
+    ];
+
+// Intern Info question Array
+const internInfo = [
+        {
+            type: "input",
+            name: "internName",
+            message: "Enter Intern's Name: "
+        },
+        {
+            type: "input",
+            name: "internID",
+            message: "Enter Intern's ID: "
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "Enter Intern's Email: "
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "Enter Intern's School Name: "
+        }
+    ];
+
+// Message to add more team members and their types
+const addMore = [
+        {
+            type: "list",
+            name: "addAnother",
+            choices: ["Engineer", "Intern", "I am done adding team members."],
+            message: "Add Another?"
+        }
+    ];
+
+inquirer.prompt(managerInfo).then(ans =>{
+    mainArr.push(new Manager(ans.managerName, ans.managerID, ans.managerEmail, ans.managerOfficeNumber));
+    promptAddMore();
+});
+
+const promptAddMore = () => {
+    inquirer.prompt(addMore).then(data => {
+        switch (data.addAnother){
+            case "Intern":
+                internQuestions();
+                break;
+            case "Engineer":
+                promptEngineer();
+                break;
+            default:
+                produceHTML();
+        }
+    });
+}
+
+const internQuestions = () => {
+    inquirer.prompt(internInfo).then(ans => {
+        mainArr.push(new Intern(ans.internName, ans.internID, ans.internEmail, ans.internSchool));
+        promptAddMore();
+    });
+}
+
+const engineerQuestions = () => {
+    inquirer.prompt(internInfo).then(ans => {
+        mainArr.push(new Intern(ans.engineerName, ans.engineerID, ans.engineerEmail, ans.engineerGithub));
+        promptAddMore();
+    });
+}
+
+const produceHTML = () => {
+
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
